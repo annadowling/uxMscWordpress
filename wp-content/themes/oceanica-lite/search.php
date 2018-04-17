98cad2069edc870c982cf4e1d2531f6ee60c8484
annadowling <anna.dowling@errigal.com>
2018-04-17 20:03:06 +0100

    Fixed search page

--- wp-content/themes/oceanica-lite/search.php
+++ wp-content/themes/oceanica-lite/search.php
@@ -1,36 +1,40 @@
 <?php
 /**
- * The template for displaying search results pages
+ * The template for displaying search results pages.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package oceanica-lite
+ * @package stackstar.
  */
+ 
 get_header(); ?>
-	<div class="wrapper main-wrapper clear">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<?php if ( have_posts() ) : ?>
-					<header class="page-header">
-						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'oceanica-lite' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-					</header><!-- .page-header -->
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
-						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'search' );
-					endwhile;
-					// Previous/next page navigation.
-					oceanica_the_posts_pagination();
-				else :
-					get_template_part( 'template-parts/content', 'none' );
-				endif; ?>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-		<?php get_sidebar(); ?>
-	</div><!-- .main-wrapper -->
-<?php get_footer();
+    <div class="search-container">
+    <section id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+        <div class="search-page-form" id="ss-search-page-form"><?php get_search_form(); ?></div>
+ 
+        <?php if ( have_posts() ) : ?>
+ 
+            <header class="page-header">
+                <span class="search-page-title"><?php printf( esc_html__( 'Search Results for: %s', stackstar ), '<span>' . get_search_query() . '</span>' ); ?></span>
+            </header><!-- .page-header -->
+ 
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+            <span class="search-post-title"><?php the_title(); ?></span>
+            <span class="search-post-excerpt"><?php the_excerpt(); ?></span>
+            <span class="search-post-link"><a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a></span>
+ 
+            <?php endwhile; ?>
+ 
+            <?php //the_posts_navigation(); ?>
+ 
+        <?php else : ?>
+ 
+            <?php //get_template_part( 'template-parts/content', 'none' ); ?>
+ 
+        <?php endif; ?>
+ 
+        </main><!-- #main -->
+    </section><!-- #primary -->
+</div>
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
